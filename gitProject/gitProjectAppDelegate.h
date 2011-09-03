5cfc91c121c43bfa405a398c8a5ddb323bf50cf7
mononos <nos.nbs@gmail.com>
2011-09-03 10:24:15 +0700

    GitHub for Mac: Throw-away commit.

---
+++ gitProject/gitProjectAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  gitProjectAppDelegate.h
+//  gitProject
+//
+//  Created by Bodin Milindankura on 9/3/54 BE.
+//  Copyright 2554 nos.nbs@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gitProjectAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
